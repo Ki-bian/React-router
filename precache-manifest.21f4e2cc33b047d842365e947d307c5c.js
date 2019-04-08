52e39f3e8cb5a94f2e6d42f09c4630933bc65dc8
Kibian <kevcass01@yahoo.fr>
2019-04-08 16:02:32 +0200

    Updates

---
+++ precache-manifest.21f4e2cc33b047d842365e947d307c5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d019f898632a3ea7021c",
+    "url": "/static/css/main.7139ef62.chunk.css"
+  },
+  {
+    "revision": "d019f898632a3ea7021c",
+    "url": "/static/js/main.4a11b293.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "972f395901c34f067411",
+    "url": "/static/js/2.ea64f2ec.chunk.js"
+  },
+  {
+    "revision": "9c416a08b9af9537071d00bdb066e009",
+    "url": "/index.html"
+  }
+];
